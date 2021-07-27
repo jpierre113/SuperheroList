8652fedec244dc0c01e35c9b3cd740bd3c3a5878
Janna Pierre <gojanna@aol.com>
2021-07-26 21:27:43 -0400

    Added Modal and Powerstats

--- src/HeroCard.tsx
+++ src/HeroCard.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Modal, Button} from 'react-bootstrap';
 
 function HeroCard(props:any) {
+  const [modal, setModal] = useState(false);
+
+  const handleModal = () => setModal(!modal);
+
     return(
       <div className='heroCard'>
-        <img src={props.images.sm} />
+        <img src={props.images.sm} alt={props.name} onClick={() => handleModal()} />
         <h3>{props.name}</h3>
+        <div>
+          <Modal show={modal} centered>
+          <Modal.Body>
+            Intelligence: {props.powerstats.intelligence} <br/>
+            Strength: {props.powerstats.strength} <br/>
+            Speed: {props.powerstats.speed} <br/>
+            Durability: {props.powerstats.durability} <br/>
+            Power: {props.powerstats.power} <br/>
+            Combat: {props.powerstats.combat} <br/>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={() => handleModal()}> Close </Button>
+          </Modal.Footer>
+          </Modal>
+      </div>
       </div>
     );
 }
