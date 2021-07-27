4603a041e1eb9c4bae57fe7b9cb500d6d4c92145
Janna Pierre <gojanna@aol.com>
2021-07-26 22:59:38 -0400

    Added class to icons, imported icons

--- src/HeroCard.tsx
+++ src/HeroCard.tsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import {Modal, Button} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBrain, faDumbbell, faTachometerAlt, faShieldAlt, faBolt, faFistRaised }  from '@fortawesome/free-solid-svg-icons';
 
 function HeroCard(props:any) {
   const [modal, setModal] = useState(false);
@@ -14,12 +16,12 @@ function HeroCard(props:any) {
           <Modal show={modal} size='sm' centered>
             <Modal.Header>{props.name}</Modal.Header>
             <Modal.Body>
-              Intelligence: {props.powerstats.intelligence} <br/>
-              Strength: {props.powerstats.strength} <br/>
-              Speed: {props.powerstats.speed} <br/>
-              Durability: {props.powerstats.durability} <br/>
-              Power: {props.powerstats.power} <br/>
-              Combat: {props.powerstats.combat} <br/>
+              <span className='statIcon'><FontAwesomeIcon icon={faBrain} /></span> Intelligence: {props.powerstats.intelligence}  <br/>
+              <span className='statIcon'><FontAwesomeIcon icon={faDumbbell} /></span> Strength: {props.powerstats.strength}<br/>
+              <span className='statIcon'><FontAwesomeIcon icon={faTachometerAlt} /></span> Speed: {props.powerstats.speed}  <br/>
+              <span className='statIcon'><FontAwesomeIcon icon={faShieldAlt} /></span> Durability: {props.powerstats.durability}  <br/>
+              <span className='statIcon'><FontAwesomeIcon icon={faBolt} /></span> Power: {props.powerstats.power} <br/>
+              <span className='statIcon'><FontAwesomeIcon icon={faFistRaised} /></span>  Combat: {props.powerstats.combat} <br/>
             </Modal.Body>
             <Modal.Footer>
               <Button onClick={() => handleModal()}> Close </Button>
